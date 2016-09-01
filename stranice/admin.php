c616b356fd0095e332833a2338de6c2bd6bc526d
Irma Karasoftić <ikarasofti1@etf.unsa.ba>
2016-09-01 16:03:48 +0200

    ispravljena imena foldera

---
+++ stranice/admin.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+
+if(isset($_SESSION['login'])){
+	$_SESSION['halid'] = 'zvjerka';
+}
+?>
+
+<!DOCTYPE HTML>
+<HTML>
+	<HEAD>
+		<TITLE>Sound Art Studio</TITLE>
+		<link rel="stylesheet" type="text/css" href="../stil.css">
+		<script src="../skripte/script.js"></script>
+		<META charset=utf-8>
+		</HEAD>
+		<BODY>
+			<div id="zaglavlje">
+				<h1>Sound Art Studio</h1>
+				<div id="logo">
+					<div id="kruzic"></div>
+					<div id="linija"></div>
+				</div>
+			</div>
+
+			<a class="skip-main" href="GlavniDioNovosti">Preskoči čitanje menija</a>
+			<div id="meni">
+				<ul>
+					<li><a href="../index.php">Naslovnica</a></li>
+					<li><a href="omeni.php">O meni</a></li>
+					<li><a href="coveri.php">Coveri</a></li>
+					<li><a href="kontakt.php">Kontakt</a></li>
+					<li><a href="login.php">Logout</a></li>
+				</ul>
+			</div>
+			<form action="dodavanjeNovosti.php">
+				<input type="submit" id="DodavanjeNovosti" value="Dodaj novost">
+			</form>
+			<?php
+			
+			if (isset($_POST['logout'])) {
+				unset($_SESSION['login']);
+				session_destroy();
+				echo '<script>alert("Uspjesno ste se izlogovali");</script>';
+				header("Location: login.php");
+			}
+
+			if(!isset($_SESSION['login'])){
+				header("Location: ../index.php");
+			}
+
+			if (isset($_SESSION['login']))
+			{
+				echo "<form action='logout.php' method='POST'><input type='submit' id='logout' name='logout' value='Logout'></form>";
+			}
+
+			?>
+		</BODY>
+	</HTML>
---
+++ stranice/kontakt.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+
+if(isset($_SESSION['login'])){
+	$_SESSION['halid'] = 'zvjerka';
+}
+?>
+<!DOCTYPE html>
+<html>
+<HEAD>
+<TITLE>Kontakt</TITLE>
+<link rel="stylesheet" type="text/css" href="../stil.css">
+<script src="../skripte/validacijaForme.js"></script>
+<META charset=utf-8>
+</HEAD>
+<body>
+
+  <div id="zaglavlje">
+  	<h1>Sound Art Studio</h1>
+  <div id="logo">
+  	<div id="kruzic"></div>
+  	<div id="linija"></div>
+  </div>
+  </div>
+  <a class="skip-main" href="kontakt">Preskoči čitanje menija</a>
+  	<div id="meni">
+  		<ul>
+  		<li><a href="../index.php">Naslovnica</a></li>
+  		<li><a href="omeni.php">O meni</a></li>
+  		<li><a href="coveri.php">Coveri</a></li>
+  		<li><a href="#">Kontakt</a></li>
+      <?php
+		if(isset($_SESSION['login'])){
+			print "<li><a href='admin.php'>Logout</a></li>";
+		}
+		else print "<li><a href='login.php'>Login</a></li>";
+		?>
+  		</ul>
+  	</div>
+    <div class="kontaktiraj">
+    <ul class="drustvenemreze">
+    <li><a class="fb" href="https://www.facebook.com/halid.hajdukovic?fref=ts" target="_blank">Facebook</a></li>
+    <li><a class="yt" href="https://www.youtube.com/channel/UCXasrB2DKH0pSv3w4Fnua4g" target="_blank">Youtube</a></li>
+    <li><a class="fb" href="https://www.instagram.com/halidhajdukovic/" target="_blank">Instagram</a></li>
+    </ul>
+  </div>
+
+<div id="kontakt">
+  <form class="FormaKontakt">
+    <div class="legendaKontakt">Kontaktirajte nas!</div>
+
+    <div id="podaci">
+    <br><br>
+    <label>Halid Hajduković</label><br><br>
+    <label>Email: halid307@gmail.com</label>
+   <br><br>
+    <label>Broj telefona: +387 61/908-118</label>
+    </div>
+  </form>
+
+</div>
+</body>
+</html>
